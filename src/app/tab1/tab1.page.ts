a4122271d33636d44ace08939340ca41d4e0fbaa
Annika von Bayern <annikawespe@gmail.com>
2023-05-17 11:34:23 +0200

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { Capacitor } from '@capacitor/core'
+import { DocumentScanner } from 'capacitor-document-scanner'
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,5 +11,20 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    scanDocument();
+  }
+
+}
+
+const scanDocument = async () => {
+  // start the document scanner
+  const { scannedImages } = await DocumentScanner.scanDocument()
+
+  // get back an array with scanned image file paths
+  if (scannedImages && scannedImages.length > 0) {
+    // set the img src, so we can view the first scanned image
+    const scannedImage = document.getElementById('scannedImage') as HTMLImageElement
+    scannedImage.src = Capacitor.convertFileSrc(scannedImages[0])
+  }
 }
